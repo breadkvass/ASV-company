ced21a26eb1e179853a794b16d5dff6e3e73473a
breadkvass <breadkvass@gmail.com>
2023-11-08 16:09:59 +0300

    add validation

--- src/components/modal-form/modal-form.jsx
+++ src/components/modal-form/modal-form.jsx
@@ -18,8 +18,70 @@ function ModalForm(props) {
 
     const [ nameValue, setNameValue ] = useState('');
     const [ emailValue, setEmailValue ] = useState('');
-    const [ emailPhone, setPhoneValue ] = useState('');
+    const [ telValue, setTelValue ] = useState('');
     const [ сommentValue, setCommentValue ] = useState('');
+
+    const nameHandler = (e) => {
+        setNameValue(e.target.value);
+        const validName = /[a-zA-Zа-яёА-ЯЁ\s\-]+$/;
+        if (!validName.test(String(e.target.value))) {
+            setinputsErrors({...inputsErrors, name: 'Используйте только буквы (не менее 2)'})
+        } else {
+            setinputsErrors({...inputsErrors, name: ''})
+        }
+    }
+
+    const emailHandler = (e) => {
+        setEmailValue(e.target.value);
+        const validEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        if (!validEmail.test(String(e.target.value))) {
+            setinputsErrors({...inputsErrors, email: 'Некорректный E-mail'})
+        } else {
+            setinputsErrors({...inputsErrors, email: ''})
+        }
+    }
+
+    const telHandler = (e) => {
+        setTelValue(e.target.value);
+        const validTel = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+        if (!validTel.test(String(e.target.value)) || e.target.value.length < 11) {
+            setinputsErrors({...inputsErrors, tel: 'Некорректный номер телефона'})
+        } else {
+            setinputsErrors({...inputsErrors, tel: ''})
+        }
+    }
+
+    const commentHandler = (e) => {
+        setCommentValue(e.target.value);
+    }
+
+    const [ inputsDirty, setInputsDirty ] = useState({
+        name: false,
+        email: false,
+        tel: false,
+    })
+
+    const [ inputsErrors, setinputsErrors ] = useState({
+        name: 'Поле не должно быть пустым',
+        email: 'Поле не должно быть пустым',
+        tel: 'Поле не должно быть пустым',
+    })
+
+
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'name':
+                setInputsDirty({...inputsDirty, name: true})
+                break
+            case 'email':
+                setInputsDirty({...inputsDirty, email: true})
+                break
+            case 'tel':
+                setInputsDirty({...inputsDirty, tel: true})
+                break
+        }
+    }
 
     useEffect(() => {
         document.addEventListener("keydown", escHandler, false);
@@ -38,34 +100,45 @@ function ModalForm(props) {
     return ReactDOM.createPortal(
         (
         <ModalOverlay closeHandler={props.closeHandler}>
-            <form className={styles.form} onClick={stopPropagation}>
+            <form className={styles.form} onClick={stopPropagation} >
                 <img className={styles.close_icon} src={closeIcon} alt="Закрыть" onClick={onClickHandler}/>
                 <h3 className={styles.title}>Оставить заявку</h3>
                 <label className={styles.label}>ФИО
                     <input className={styles.input}
+                    onBlur={e => blurHandler(e)}
+                    name='name'
                     type="text"
-                    onChange={(e) => setNameValue(e.target.value)}
+                    onChange={e => nameHandler(e)}
                     value={nameValue}
                     />
+                    {(inputsDirty.name && inputsErrors.name) && <p className={styles.error}>{inputsErrors.name}</p>}
                 </label>
                 <label className={styles.label}>E-mail
                     <input className={styles.input}
+                    onBlur={e => blurHandler(e)}
                     type="email"
-                    onChange={(e) => setEmailValue(e.target.value)}
+                    name='email'
+                    onChange={e => emailHandler(e)}
                     value={emailValue}
                     />
+                    {(inputsDirty.email && inputsErrors.email) && <p className={styles.error}>{inputsErrors.email}</p>}
                 </label>
                 <label className={styles.label}>Телефон
                     <input className={styles.input}
+                    onBlur={e => blurHandler(e)}
                     type="tel"
-                    onChange={(e) => setPhoneValue(e.target.value)}
-                    value={emailPhone}
+                    name='tel'
+                    pattern='(\+?\d[- .]*){7,13}' required
+                    onChange={e => telHandler(e)}
+                    value={telValue}
                     placeholder='8 (999) 123-45-67'
+                    
                     />
+                    {(inputsDirty.tel && inputsErrors.tel) && <p className={styles.error}>{inputsErrors.tel}</p>}
                 </label>
                 <label className={styles.label}>Комментарий
                     <textarea className={styles.textarea}
-                    onChange={(e) => setCommentValue(e.target.value)}
+                    onChange={e => setCommentValue(e.target.value)}
                     value={сommentValue}
                     placeholder='При желании укажите необходимые сроки или другую важную для вас информацию'
                     />
