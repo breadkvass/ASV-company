60e517bd0419f8f0294b224e0ef2b3882520d300
Anastasiya Sulimova <breadkvass@gmail.com>
2023-11-08 12:55:22 +0300

    webpack svg

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
           ],
         },
         {
-          test: /\.(jpg|png)$/,
+          test: /\.(png|jp(e*)g|svg|gif)$/,
           use: {
             loader: 'url-loader',
           },
