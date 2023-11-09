e8e7fefc7eb6a07b9cf3a4f616f335fec7e82138
breadkvass <breadkvass@gmail.com>
2023-11-09 15:17:41 +0300

    upd php

--- asv-requests.php
+++ asv-requests.php
@@ -1,33 +1,28 @@
 <?php
+$txt = null;
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
 
 $token = "6516410091:AAHJBsEdu8P5cDAZJ7Ud5Ruh-i54kugoptM";
-
 $chat_id = "-1002134144417";
-
-$name = ($_POST['name']);
-$email = ($_POST['email']);
-$phone = ($_POST['phone']);
-$comment = ($_POST['comment']);
-
 $arr = array(
-    'Имя:' => $name,
-    'Почта:' => $email,
-    'Телефон:' => $phone,
-    'Комментарий:' => $comment
+  'ФИО: ' => $name,
+  'Email' => $email,
+  'Телефон: ' => $tel,
+  'Комментарий: ' => $comment,
 );
 
 foreach($arr as $key => $value) {
-    $txt .= "<b>".$key."</b> ".$value."%0A";
+  $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-    alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
+    echo "Успешно";
+} else {
+    echo "Error";
 }
-
-else {
-    alert('Что-то пошло не так. Попробуйте отправить форму ещё раз.');
-}
-
 ?>
